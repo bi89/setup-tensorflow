9f3fd6f932cd9c070a3f761ef0ec906975d731b4
Suchathit Boonnag <bi89@Suchathits-MacBook-Pro.local>
2017-06-14 11:12:38 +0700

    update

---
+++ bashrc-init.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+user=$(whoami)
+touch /home/users/$user/.bashrc
+
+echo -n "Hello $user"
+echo ", It will export Miniconda3, CUDA 8.0, CUDA 8.0 Lib64."
+echo -n 'export PATH="/home/users/' >> /home/users/$user/.bashrc
+echo -n "$user" >> /home/users/$user/.bashrc
+echo '/miniconda3/bin:$PATH"' >> /home/users/$user/.bashrc
+echo "Add Miniconda3 Done!"
+echo 'export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}' >> /home/users/$user/.bashrc
+echo "Add CUDA 8.0 Done!"
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64\' >> /home/users/$user/.bashrc
+echo '                    ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> /home/users/$user/.bashrc
+echo "Add CUDA 8.0 Lib64 Done!"
+echo "===="
+echo "All Done!"
+echo "===="
+exit
---
+++ ssh-forward.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ "$#" -eq  "1" ]
+then
+	echo Start ssh forward with user $1
+	ssh -L 6606:127.0.0.1:6606 -N -f $1@osaka.siitai.xyz
+	ssh -L 8888:127.0.0.1:8888 -N -f $1@osaka.siitai.xyz
+else
+	echo "Invalid argument: bash ss-foward.bash {Osaka username}"
+fi
+exit
