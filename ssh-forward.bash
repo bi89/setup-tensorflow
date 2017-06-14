59e3147b52a97e4979b937113e766171d0f187df
Suchathit Boonnag <bi1989@gmail.com>
2017-06-14 12:28:17 +0700

    update1

--- ssh-forward.bash
+++ ssh-forward.bash
@@ -5,6 +5,6 @@ then
 	ssh -L 6606:127.0.0.1:6606 -N -f $1@osaka.siitai.xyz
 	ssh -L 8888:127.0.0.1:8888 -N -f $1@osaka.siitai.xyz
 else
-	echo "Invalid argument: bash ss-foward.bash {Osaka username}"
+	echo "Invalid argument: bash ssh-foward.bash {Osaka username}"
 fi
 exit
